049387dec8f27eaba693fa2d4ba734b2d13c4f5a
ashidiqidimas <prabowo6@gmail.com>
2021-11-13 15:28:19 +0700

    Added and configured tabbar

--- Spotify/Controller/Core/TabBarViewController.swift
+++ Spotify/Controller/Core/TabBarViewController.swift
@@ -12,8 +12,44 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+		setupControllers()
+		setupTabBar()
     }
+	
+	func setupTabBar() {
+		let tabBarAppearance = UITabBarAppearance()
+		tabBarAppearance.configureWithOpaqueBackground()
+		tabBarAppearance.backgroundColor = .systemBackground
+		tabBar.standardAppearance = tabBarAppearance
+		tabBar.scrollEdgeAppearance = tabBar.standardAppearance
+		tabBar.isTranslucent = true
+		tabBar.unselectedItemTintColor = .secondaryLabel
+	}
+	
+	func setupControllers() {
+		let homeViewController = HomeViewController()
+		homeViewController.title = "Browse"
+		homeViewController.navigationItem.largeTitleDisplayMode = .always
+		let homeViewNav = UINavigationController(rootViewController: homeViewController)
+		homeViewNav.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
+		homeViewNav.navigationBar.prefersLargeTitles = true
+		
+		let searchViewController = SearchViewController()
+		searchViewController.title = "Search"
+		searchViewController.navigationItem.largeTitleDisplayMode = .always
+		let searchViewNav = UINavigationController(rootViewController: searchViewController)
+		searchViewNav.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "magnifyingglass"), tag: 2)
+		searchViewNav.navigationBar.prefersLargeTitles = true
+		
+		let libraryViewController = LibraryViewController()
+		libraryViewController.title = "Library"
+		libraryViewController.navigationItem.largeTitleDisplayMode = .always
+		let libraryViewNav = UINavigationController(rootViewController: libraryViewController)
+		libraryViewNav.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "book"), tag: 3)
+		libraryViewNav.navigationBar.prefersLargeTitles = true
+		
+		setViewControllers([homeViewNav, searchViewNav, libraryViewNav], animated: false)
+	}
     
 
     /*
